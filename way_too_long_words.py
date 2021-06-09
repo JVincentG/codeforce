73208f1b292fe38725bfd0d4e48aa9d98bedc971
Vincent <59752567+Ragnov@users.noreply.github.com>
2021-06-09 17:05:10 +0800

    Delete way_too_long_words.py

--- way_too_long_words.py
+++
@@ -1,7 +0,0 @@
-num_of_words = int(input())
-for i in range(num_of_words):
-    word = input()
-    if len(word) > 10:
-        print (word[0] + str(len(word)-2) + word[-1])
-    else:
-        print(word)
